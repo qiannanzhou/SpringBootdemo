3c33ae6d0721e6c38057e9a974aaf9959b6a20aa
zhouqiannan4 <zhouqiannan4@guazi.com>
2021-04-02 00:12:20 +0800

    11

--- src/main/java/com/example/demo/learning/InterviewTest.java
+++ src/main/java/com/example/demo/learning/InterviewTest.java
@@ -31,7 +31,7 @@ public class InterviewTest {
 //        cloneModel11.setProperty1("clone11");
 //
 //        System.out.println("cloneModel1"+cloneModel1);
-//        System.out.println("cloneModel11"+cloneModel11);
+//        System.out.println("cloneModel11"+cloneModel11);111
 
 //Object默认的clone方法实际是对域的简单拷贝，对于简单数据类型，是值的拷贝，对于复杂类型的字段，则是指针地址的拷贝，clone后的对象和原对象指向的还是一个地址空间，所以说默认的clone方法时浅克隆。
         System.out.println("clone方法对对象中包含引用类型 clone的是一个新的对象，但其中的引用类型是指向的同一地址，所以改变clone后对象的引用的数据，之前的也会变化，否则则需要讲clone的对象也实现Cloneable方法");
